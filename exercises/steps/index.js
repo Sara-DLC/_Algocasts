512374e8466fc988c6ec455c2b5791f46dd63f21
Sara De La Cruz <54650844+Sara-DLC@users.noreply.github.com>
2020-06-26 22:09:22 -0500

    steps solutions

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,6 +17,51 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+
+//pseudo code:
+    // From 0 to n (iterate through rows)
+        // Create and empty String, "stair"
+        // From 0 to n (iterate through columns)
+            //IF the current column is equal to or less than the current row
+                //Add a "#" to "stair"
+            //ELSE
+                //Add a space to "stair"
+        //Console log "stair"
+
+// function steps(n) {
+//     for (let row = 0; row < n; row ++){
+//         let stair = "";
+        
+//         for (let column = 0; column < n; column++){
+//             if (column <=row){
+//                 stair += "#";
+//             }
+//             else {
+//                 stair += " ";
+//             }
+//         }
+//         console.log(stair);
+//     }
+// }
+
+//recursive solution
+
+function steps(n, row = 0, stair = '') {
+    if ( row === n) {
+        return;
+    } 
+
+    if (n === stair.length){
+        console.log(stair);
+        return steps(n, row + 1);
+    }
+
+    if (stair.length <= row) {
+        stair += "#";
+    } else {
+        stair += " ";
+    }
+    steps(n, row, stair);
+}
 
 module.exports = steps;
